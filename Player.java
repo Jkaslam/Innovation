e4aabb1a8228f6c656656c7bb2d7e99f2a83aa07
jkaslam <jaikaslam@gmail.com>
2017-05-02 23:05:17 -0400

    added score

--- Player.java
+++ Player.java
@@ -1,5 +1,23 @@
 public class Player {
+  private String name;
   private Map<Color, Pile> piles;
   private Map<Symbol, Integer> numSymbols;
+  private ArrayList<Card> scorePile;
+  private ArrayList<Achievement> achievements;
+  
+  public Player(String name) {
+    this.name = name;
+	piles = new Hashtable<Color, Pile>;
+	numSymbols = new Hashtable<Symbol, Integer>;
+	scorePile = new ArrayList<Card>();
+	achievements = new ArrayList<Achievement>();
+  }
+  
+  public int getScore() {
+    int score = 0;
+	for (card : scorePile) {
+	  score += card.getAge();
+	}
+  }
   
 }
